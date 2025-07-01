b4728b5a9873cfb538edf0b12413216ef97bf16f
KumarShresth7 <kumarshresth2004@gmail.com>
2025-07-01 12:28:52 +0530

    endpoint changed

--- client/config/api.ts
+++ client/config/api.ts
@@ -1,2 +1,2 @@
-const API_BASE_URL = 'https://c231-2405-201-500c-50c8-5042-bc6a-81c2-37ef.ngrok-free.app';
+const API_BASE_URL = 'https://1337-122-162-149-122.ngrok-free.app';
 export default API_BASE_URL;
